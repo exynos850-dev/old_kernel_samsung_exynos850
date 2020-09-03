6e91a47de0ed7fc6ec6940869174ce431372e5dc
Tim Zimmermann <tim@linux4.de>
2020-09-03 09:47:13 +0200

    security: remove samsung sdp
    Change-Id: I17500bf770d72c4454055f0ce592fae7a655e2fd

--- fs/crypto/crypto.c
+++ fs/crypto/crypto.c
@@ -26,9 +26,6 @@
 #include <linux/ratelimit.h>
 #include <crypto/skcipher.h>
 #include "fscrypt_private.h"
-#ifdef CONFIG_FSCRYPT_SDP
-#include "sdp/sdp_crypto.h"
-#endif
 
 static unsigned int num_prealloc_crypto_pages = 32;
 
@@ -111,9 +108,6 @@ int fscrypt_crypt_block(const struct inode *inode, fscrypt_direction_t rw,
 	struct fscrypt_info *ci = inode->i_crypt_info;
 	struct crypto_skcipher *tfm = ci->ci_key.tfm;
 	int res = 0;
-#ifdef CONFIG_FSCRYPT_SDP
-	sdp_fs_command_t *cmd = NULL;
-#endif
 
 	if (WARN_ON_ONCE(len <= 0))
 		return -EINVAL;
@@ -143,26 +137,6 @@ int fscrypt_crypt_block(const struct inode *inode, fscrypt_direction_t rw,
 	if (res) {
 		fscrypt_err(inode, "%scryption failed for block %llu: %d",
 			    (rw == FS_DECRYPT ? "De" : "En"), lblk_num, res);
-#ifdef CONFIG_FSCRYPT_SDP
-		if (ci->ci_sdp_info) {
-			if (ci->ci_sdp_info->sdp_flags & SDP_DEK_IS_SENSITIVE) {
-				printk("Record audit log in case of a failure during en/decryption of sensitive file\n");
-				if (rw == FS_DECRYPT) {
-					cmd = sdp_fs_command_alloc(FSOP_AUDIT_FAIL_DECRYPT,
-					current->tgid, ci->ci_sdp_info->engine_id, -1, inode->i_ino, res,
-							GFP_KERNEL);
-				} else {
-					cmd = sdp_fs_command_alloc(FSOP_AUDIT_FAIL_ENCRYPT,
-					current->tgid, ci->ci_sdp_info->engine_id, -1, inode->i_ino, res,
-							GFP_KERNEL);
-				}
-				if (cmd) {
-					sdp_fs_request(cmd, NULL);
-					sdp_fs_command_free(cmd);
-				}
-			}
-		}
-#endif
 		return res;
 	}
 	return 0;
@@ -410,11 +384,6 @@ static int __init fscrypt_init(void)
 	if (err)
 		goto fail_free_info;
 
-#ifdef CONFIG_FSCRYPT_SDP
-	sdp_crypto_init();
-	if (!fscrypt_sdp_init_sdp_info_cachep())
-		goto fail_free_info;
-#endif
 	return 0;
 
 fail_free_info:
