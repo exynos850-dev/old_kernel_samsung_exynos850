d7ab9befc31ae0e5f7b1a82eddd0c8f8a2d11b20
apass3rby <100168186+apass3rby@users.noreply.github.com>
2022-04-11 17:46:33 +0300

    input: touchscreen: nt36525: Invert reported proximity values
    * On stock higher value indicates closer proximity while on AOSP it is expected to be the other way around
    
    Change-Id: Ia4e95f2c656397fe58872a5d6bcacdc614418bb7

--- drivers/input/touchscreen/novatek/nt36525/nt36xxx.c
+++ drivers/input/touchscreen/novatek/nt36525/nt36xxx.c
@@ -1729,6 +1729,7 @@ void nvt_ts_proximity_report(uint8_t *data)
 	}
 
 	status = p_event_proximity->status;
+	status = !status;
 
 	input_info(true, &ts->client->dev,"proximity->status = %d\n", status);
 
